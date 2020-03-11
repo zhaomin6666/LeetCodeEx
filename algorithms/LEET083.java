63f148b3da4d187a9b0b12f3a4d35b30b75aaf3e
zhaomin6666 <804122845@qq.com>
2020-03-11 11:47:49 +0800

    新增82,83,1013已完成，TreeNode增加泛型（提交答案时需要删除）

---
+++ algorithms/LEET083.java
@@ -0,0 +1,50 @@
+package com.zm.LeetCodeEx.algorithms;
+
+import com.zm.LeetCodeEx.CommonFunctions;
+import com.zm.LeetCodeEx.ListNode;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ * <p>
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * <p>
+ * 示例 1: <br>
+ * 输入: 1->1->2 <br>
+ * 输出: 1->2
+ * <p>
+ * 示例 2: <br>
+ * 输入: 1->1->2->3->3 <br>
+ * 输出: 1->2->3
+ * 
+ * @author zm
+ */
+public class LEET083 {
+	public static void main(String[] args) {
+		LEET083 l082 = new LEET083();
+		System.out.println(CommonFunctions.listNodeToString(
+				l082.new Solution().deleteDuplicates(CommonFunctions.stringToListNode("[1,2,3,3,4,4,5]"))));
+		System.out.println(CommonFunctions.listNodeToString(
+				l082.new Solution().deleteDuplicates(CommonFunctions.stringToListNode("[1,1,1,2,3]"))));
+		System.out.println(CommonFunctions.listNodeToString(
+				l082.new Solution().deleteDuplicates(CommonFunctions.stringToListNode("[1,1,1,2,2,3,3]"))));
+		System.out.println(CommonFunctions.listNodeToString(
+				l082.new Solution().deleteDuplicates(CommonFunctions.stringToListNode("[1,1,2,2,2,2,2,3,4,4,5]"))));
+	}
+
+	class Solution {
+		public ListNode<Integer> deleteDuplicates(ListNode<Integer> head) {
+			if (head == null) {
+				return null;
+			}
+			ListNode<Integer> temp = head;
+			while (temp.next != null) {
+				if (temp.next.val == temp.val) {
+					temp.next = temp.next.next;
+				}else {
+					temp = temp.next;
+				}
+			}
+			return head;
+		}
+	}
+}
