495a3e2491fa23ae8d12caa7674bc92f6787a5cf
zhaomin6666 <804122845@qq.com>
2020-04-05 00:31:16 +0800

    新增107,108,109已完成

---
+++ algorithms/ex101_200/LEET107.java
@@ -0,0 +1,103 @@
+package com.zm.LeetCodeEx.algorithms.ex101_200;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import com.alibaba.fastjson.JSON;
+import com.zm.LeetCodeEx.CommonFunctions;
+import com.zm.LeetCodeEx.TreeNode;
+
+/**
+ * 107. 二叉树的层次遍历 II
+ * <p>
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。.（即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * <p>
+ * 例如：<br>
+ * 给定二叉树.[3,9,20,null,null,15,7],<br>
+ * 
+ * ....3 <br>
+ * .../.\ <br>
+ * ..9..20 <br>
+ * ..../..\ <br>
+ * ...15...7 <br>
+ * 返回其自底向上的层次遍历为： <br>
+ * 
+ * [ <br>
+ * ..[15,7], <br>
+ * ..[9,20], <br>
+ * ..[3] <br>
+ * ]
+ * 
+ * @author zm
+ */
+public class LEET107 {
+	public static void main(String[] args) {
+		LEET107 l107 = new LEET107();
+		System.out.println(JSON.toJSONString(
+				l107.new Solution2().levelOrderBottom(CommonFunctions.stringToTreeNode("[3,9,20,null,null,15,7]"))));
+
+	}
+
+	class Solution {
+		public List<List<Integer>> levelOrderBottom(TreeNode<Integer> root) {
+			if (root == null) {
+				return new LinkedList<>();
+			}
+			Stack<List<TreeNode<Integer>>> stack = new Stack<>();
+			List<TreeNode<Integer>> list = new LinkedList<TreeNode<Integer>>();
+			list.add(root);
+			stack.add(list);
+			while (true) {
+				List<TreeNode<Integer>> listTemp = new LinkedList<TreeNode<Integer>>();
+				List<TreeNode<Integer>> listLast = stack.peek();
+				for (TreeNode<Integer> treeNode : listLast) {
+					if (treeNode.left != null) {
+						listTemp.add(treeNode.left);
+					}
+					if (treeNode.right != null) {
+						listTemp.add(treeNode.right);
+					}
+				}
+				if (listTemp.isEmpty()) {
+					break;
+				} else {
+					stack.add(listTemp);
+				}
+			}
+			LinkedList<List<Integer>> ret = new LinkedList<>();
+			while (!stack.isEmpty()) {
+				List<TreeNode<Integer>> temp = stack.pop();
+				List<Integer> item = new LinkedList<>();
+				for (TreeNode<Integer> treeNode : temp) {
+					item.add(treeNode.val);
+				}
+				ret.add(item);
+			}
+			return ret;
+		}
+	}
+
+	class Solution2 {
+		private LinkedList<List<Integer>> ret = new LinkedList<List<Integer>>();
+
+		public List<List<Integer>> levelOrderBottom(TreeNode<Integer> root) {
+			helper(root, 0);
+			return ret;
+		}
+
+		private void helper(TreeNode<Integer> node, int i) {
+			if (node != null) {
+				if (ret.size() <= i) {
+					List<Integer> row = new LinkedList<>();
+					row.add(node.val);
+					ret.addFirst(row);
+				} else {
+					ret.get(ret.size() - i - 1).add(node.val);
+				}
+				helper(node.left, i + 1);
+				helper(node.right, i + 1);
+			}
+		}
+	}
+}
---
+++ algorithms/ex101_200/LEET109.java
@@ -0,0 +1,60 @@
+package com.zm.LeetCodeEx.algorithms.ex101_200;
+
+import java.util.ArrayList;
+
+import com.zm.LeetCodeEx.CommonFunctions;
+import com.zm.LeetCodeEx.ListNode;
+import com.zm.LeetCodeEx.TreeNode;
+
+/**
+ * 109. 有序链表转换二叉搜索树
+ * <p>
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ * 
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过.1。 <br>
+ * 
+ * 示例: <br>
+ * 
+ * 给定的有序链表： [-10, -3, 0, 5, 9], <br>
+ * 
+ * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树： <br>
+ * 
+ * ......0 <br>
+ * ...../.\ <br>
+ * ...-3...9 <br>
+ * .../.../ <br>
+ * .-10..5 <br>
+ * 
+ * 
+ * @author zm
+ */
+public class LEET109 {
+	public static void main(String[] args) {
+		LEET109 l109 = new LEET109();
+		System.out.println(CommonFunctions.treeNodeToString(
+				l109.new Solution().sortedListToBST(CommonFunctions.stringToListNode("[-10, -3, 0, 5, 9]"))));
+	}
+
+	class Solution {
+		public TreeNode<Integer> sortedListToBST(ListNode<Integer> head) {
+			while (head != null) {
+				nums.add(head.val);
+				head = head.next;
+			}
+			return helper(0, nums.size() - 1);
+		}
+
+		private ArrayList<Integer> nums = new ArrayList<>();
+
+		private TreeNode<Integer> helper(int l, int r) {
+			if (l > r) {
+				return null;
+			}
+			int mid = (l + r + 1) >>> 1;
+			TreeNode<Integer> node = new TreeNode<>(nums.get(mid));
+			node.left = helper(l, mid - 1);
+			node.right = helper(mid + 1, r);
+			return node;
+		}
+	}
+}
