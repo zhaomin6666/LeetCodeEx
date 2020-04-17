32911ea881bd21998efd16c6fd755a29d650b4e7
zm <804122845@qq.com>
2020-04-17 21:02:59 +0800

    55新增解法

--- algorithms/ex1_100/LEET055.java
+++ algorithms/ex1_100/LEET055.java
@@ -21,45 +21,73 @@ package com.zm.LeetCodeEx.algorithms.ex1_100;
  * 输出: false<br>
  * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
  *
- *
- * 
  * @author zm
  */
 public class LEET055 {
-	public static void main(String[] args) {
-		LEET055 l055 = new LEET055();
-		int[] nums = { 2, 3, 1, 1, 4 };
-		int[] nums2 = { 3, 2, 1, 0, 4 };
-		System.out.println(l055.canJump(nums));
-		System.out.println(l055.canJump(nums2));
-	}
+    public static void main(String[] args) {
+        LEET055 l055 = new LEET055();
+        int[] nums = {2, 3, 1, 1, 4};
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println(l055.canJump(nums));
+        System.out.println(l055.canJump(nums2));
+    }
 
-	/**
-	 * 参考跳跃游戏Ⅱ
-	 * 
-	 * @param nums
-	 * @return
-	 */
-	public boolean canJump(int[] nums) {
-		if (nums.length == 1) {
-			return true;
-		}
-		int reach = 0;
-		int nextreach = nums[0];
-		for (int i = 0; i < nums.length; i++) {
-			if (reach < i) {
-				return false;
-			}
-			nextreach = Math.max(i + nums[i], nextreach);
-			if (nextreach >= nums.length - 1)
-				return true;
-			if (i == reach) {
-				reach = nextreach;
-			}
-		}
-		if (reach >= nums.length - 1) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * 参考跳跃游戏Ⅱ
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        int reach = 0;
+        int nextreach = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (reach < i) {
+                return false;
+            }
+            nextreach = Math.max(i + nums[i], nextreach);
+            if (nextreach >= nums.length - 1) {
+                return true;
+            }
+            if (i == reach) {
+                reach = nextreach;
+            }
+        }
+        if (reach >= nums.length - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 当一个0的前面所有数都跳不到这个0的后面，那么就跳不出这个数组
+     */
+    class Solution2 {
+        public boolean canJump(int[] nums) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == 0) {
+                    if (passZero(nums, i)) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        //判断是否能跳出当前0
+        private boolean passZero(int[] nums, int index) {
+            for (int i = index; i >= 0; i--) {
+                if (nums[i] > (index - i)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
