87f9213b7b37bfcd8c962121053940c10229a5b1
zhaomin6666 <804122845@qq.com>
2020-05-03 00:38:13 +0800

    新增优化解法

--- algorithms/ex1_100/LEET003.java
+++ algorithms/ex1_100/LEET003.java
@@ -11,66 +11,91 @@ import java.util.HashMap;
 public class LEET003 {
 	public static void main(String[] args) {
 		LEET003 l003 = new LEET003();
-		System.out.println(l003.lengthOfLongestSubstring("abcabcbb"));// 3
-		System.out.println(l003.lengthOfLongestSubstring("tmmzuxt"));// 5
-		System.out.println(l003.lengthOfLongestSubstring2("aabb"));// 2
-		System.out.println(l003.lengthOfLongestSubstring2("dvdf"));// 3
-		System.out.println(l003.lengthOfLongestSubstring2("aab"));// 2
-		System.out.println(l003.lengthOfLongestSubstring2("au"));// 2
-		System.out.println(l003.lengthOfLongestSubstring2("a"));// 1
-		System.out.println(l003.lengthOfLongestSubstring2(""));// 0
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("abcabcbb"));// 3
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("tmmzuxt"));// 5
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("aabb"));// 2
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("dvdf"));// 3
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("aab"));// 2
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("au"));// 2
+		System.out.println(l003.new Solution().lengthOfLongestSubstring("a"));// 1
+		System.out.println(l003.new Solution().lengthOfLongestSubstring(""));// 0
 	}
 
-	/**
-	 * 循环遍历字符串，把子串的初始位置设为-1（因为第一个是0，这样第一个字符的时候长度就是0-（-1）=1）。没有重复的时候长度就+1，并且判断一下是否是最长的数，保存一下。
-	 * 当有重复的时候，判断重复的第一个字符所在位置和当前起始位置，取较后面的为子串的初始位置。
-	 * 
-	 * @param s
-	 * @return
-	 */
-	public int lengthOfLongestSubstring(String s) {
-		HashMap<Character, Integer> hashMap = new HashMap<>();
-		int max = 0;
-		int last = -1;
-		for (int i = 0; i < s.length(); i++) {
-			char charitem = s.charAt(i);
-			if (!hashMap.containsKey(charitem)) {
-				hashMap.put(charitem, i);
-				int len = i - last;
-				if (len > max)
-					max = len;
-			} else {
-				if (hashMap.get(charitem) > last) {
-					last = hashMap.get(charitem);
-				} else {
+	class Solution {
+		/**
+		 * 循环遍历字符串，把子串的初始位置设为-1（因为第一个是0，这样第一个字符的时候长度就是0-（-1）=1）。没有重复的时候长度就+1，并且判断一下是否是最长的数，保存一下。
+		 * 当有重复的时候，判断重复的第一个字符所在位置和当前起始位置，取较后面的为子串的初始位置。
+		 * 
+		 * @param s
+		 * @return
+		 */
+		public int lengthOfLongestSubstring(String s) {
+			HashMap<Character, Integer> hashMap = new HashMap<>();
+			int max = 0;
+			int last = -1;
+			for (int i = 0; i < s.length(); i++) {
+				char charitem = s.charAt(i);
+				if (!hashMap.containsKey(charitem)) {
+					hashMap.put(charitem, i);
 					int len = i - last;
 					if (len > max)
 						max = len;
+				} else {
+					if (hashMap.get(charitem) > last) {
+						last = hashMap.get(charitem);
+					} else {
+						int len = i - last;
+						if (len > max)
+							max = len;
+					}
+					hashMap.replace(charitem, i);
 				}
-				hashMap.replace(charitem, i);
 			}
+			return max;
 		}
-		return max;
+	}
+
+	class Solution2 {
+		/**
+		 * 滑动窗口（官方题解） 貌似和上面的方法思路差不多，但是优化了代码
+		 * 
+		 * @param s
+		 * @return
+		 */
+		public int lengthOfLongestSubstring2(String s) {
+			int max = 0;
+			int left = -1;
+			HashMap<Character, Integer> hashMap = new HashMap<>();
+			for (int i = 0; i < s.length(); i++) {
+				char charitem = s.charAt(i);
+				if (hashMap.containsKey(charitem)) {
+					left = Math.max(left, hashMap.get(charitem));
+				}
+				max = Math.max(i - left, max);
+				hashMap.put(charitem, i);
+			}
+			return max;
+		}
 	}
 
 	/**
-	 * 滑动窗口（官方题解） 貌似和上面的方法思路差不多，但是优化了代码
+	 * 用数组代替hashmap
 	 * 
-	 * @param s
-	 * @return
+	 * @author zm
+	 *
 	 */
-	public int lengthOfLongestSubstring2(String s) {
-		int max = 0;
-		int left = -1;
-		HashMap<Character, Integer> hashMap = new HashMap<>();
-		for (int i = 0; i < s.length(); i++) {
-			char charitem = s.charAt(i);
-			if (hashMap.containsKey(charitem)) {
-				left = Math.max(left, hashMap.get(charitem));
+	class Solution3 {
+		public int lengthOfLongestSubstring(String s) {
+			int n = s.length();
+			int[] index = new int[128];
+			int i = 0;
+			int maxLen = 0;
+			for (int j = 0; j < n; j++) {
+				i = Math.max(index[s.charAt(j)], i);
+				maxLen = Math.max(maxLen, j - i + 1);
+				index[s.charAt(j)] = j + 1;
 			}
-			max = Math.max(i - left, max);
-			hashMap.put(charitem, i);
+			return maxLen;
 		}
-		return max;
 	}
 }
