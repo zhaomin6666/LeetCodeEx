47dd1a31fb028a2cc4cde0977e7727b976ab82f8
zhaomin6666 <804122845@qq.com>
2020-05-20 12:28:49 +0800

    5377新增解法

--- weekcontest/before20200426/LEET5337.java
+++ weekcontest/before20200426/LEET5337.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * 双周赛 2020年3月7日 5337. 每个元音包含偶数次的最长子字符串
+ * 题库收录为 No.1371
  * <p>
  * 给你一个字符串 s ，请你返回满足以下条件的最长子字符串的长度：每个元音字母，即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
  * <p>
@@ -84,6 +85,36 @@ public class LEET5337 {
                 key |= (1 << i) * (c % 2);
             }
             return key;
+        }
+    }
+
+    class Solution2 {
+        public int findTheLongestSubstring(String s) {
+            int n = s.length();
+            int[] pos = new int[1 << 5];
+            Arrays.fill(pos, -1);
+            int ans = 0, status = 0;
+            pos[0] = 0;
+            for (int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+                if (ch == 'a') {
+                    status ^= 1;
+                } else if (ch == 'e') {
+                    status ^= (1 << 1);
+                } else if (ch == 'i') {
+                    status ^= (1 << 2);
+                } else if (ch == 'o') {
+                    status ^= (1 << 3);
+                } else if (ch == 'u') {
+                    status ^= (1 << 4);
+                }
+                if (pos[status] >= 0) {
+                    ans = Math.max(ans, i + 1 - pos[status]);
+                } else {
+                    pos[status] = i + 1;
+                }
+            }
+            return ans;
         }
     }
 }
