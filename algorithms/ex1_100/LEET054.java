86a9c0b37a3140c9fd386cbaf923d49193cb9947
zhaomin6666 <804122845@qq.com>
2020-06-05 13:41:16 +0800

    54新增解法

--- algorithms/ex1_100/LEET054.java
+++ algorithms/ex1_100/LEET054.java
@@ -30,124 +30,161 @@ import java.util.List;
  * ]<br>
  * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
  *
- *
  * @author zm
  */
 public class LEET054 {
-	public static void main(String[] args) {
-		LEET054 l054 = new LEET054();
-		int[][] matrix1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-		int[][] matrix2 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-		int[][] matrix3 = { { 1, 2, 3 } };
-		System.out.println(l054.spiralOrder(matrix1));
-		System.out.println(l054.spiralOrder(matrix2));
-		System.out.println(l054.spiralOrder2(matrix3));
-	}
+    public static void main(String[] args) {
+        LEET054 l054 = new LEET054();
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] matrix3 = {{1, 2, 3}};
+        System.out.println(l054.new Solution().spiralOrder(matrix1));
+        System.out.println(l054.new Solution().spiralOrder(matrix2));
+        System.out.println(l054.new Solution().spiralOrder(matrix3));
+    }
 
-	/**
-	 * 模拟螺旋过程，碰壁之后转向
-	 * 
-	 * @param matrix
-	 * @return
-	 */
-	public List<Integer> spiralOrder(int[][] matrix) {
-		if (matrix.length == 0 || matrix[0].length == 0) {
-			return Collections.emptyList();
-		}
-		int rows = matrix.length;
-		int cols = matrix[0].length;
-		List<Integer> retList = new ArrayList<Integer>();
-		int direction = 0; // →:0,↓:1,←:2,↑:3
-		boolean[][] flagMatrix = new boolean[matrix.length][matrix[0].length];
-		int x = 0;
-		int y = 0;
+    class Solution {
+        /**
+         * 模拟螺旋过程，碰壁之后转向
+         *
+         * @param matrix
+         * @return
+         */
+        public List<Integer> spiralOrder(int[][] matrix) {
+            if (matrix.length == 0 || matrix[0].length == 0) {
+                return Collections.emptyList();
+            }
+            int rows = matrix.length;
+            int cols = matrix[0].length;
+            List<Integer> retList = new ArrayList<Integer>();
+            int direction = 0; // →:0,↓:1,←:2,↑:3
+            boolean[][] flagMatrix = new boolean[matrix.length][matrix[0].length];
+            int x = 0;
+            int y = 0;
 
-		loop: while (true) {
-			retList.add(matrix[x][y]);
-			flagMatrix[x][y] = true;
-			switch (direction) {
-			case 0:
-				if (y + 1 == cols || flagMatrix[x][y + 1]) {
-					if (x + 1 == rows || flagMatrix[x + 1][y]) {
-						break loop;
-					}
-					x++;
-					direction = 1;
-				} else {
-					y++;
-				}
-				break;
-			case 1:
-				if (x + 1 == rows || flagMatrix[x + 1][y]) {
-					if (y - 1 == -1 || flagMatrix[x][y - 1]) {
-						break loop;
-					}
-					y--;
-					direction = 2;
-				} else {
-					x++;
-				}
-				break;
-			case 2:
-				if (y - 1 == -1 || flagMatrix[x][y - 1]) {
-					if (x - 1 == -1 || flagMatrix[x - 1][y]) {
-						break loop;
-					}
-					x--;
-					direction = 3;
-				} else {
-					y--;
-				}
-				break;
-			case 3:
-				if (x - 1 == -1 || flagMatrix[x - 1][y]) {
-					if (y + 1 == cols || flagMatrix[x][y + 1]) {
-						break loop;
-					} else {
-						y++;
-					}
-					direction = 0;
-				} else {
-					x--;
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		return retList;
-	}
+            loop:
+            while (true) {
+                retList.add(matrix[x][y]);
+                flagMatrix[x][y] = true;
+                switch (direction) {
+                    case 0:
+                        if (y + 1 == cols || flagMatrix[x][y + 1]) {
+                            if (x + 1 == rows || flagMatrix[x + 1][y]) {
+                                break loop;
+                            }
+                            x++;
+                            direction = 1;
+                        } else {
+                            y++;
+                        }
+                        break;
+                    case 1:
+                        if (x + 1 == rows || flagMatrix[x + 1][y]) {
+                            if (y - 1 == -1 || flagMatrix[x][y - 1]) {
+                                break loop;
+                            }
+                            y--;
+                            direction = 2;
+                        } else {
+                            x++;
+                        }
+                        break;
+                    case 2:
+                        if (y - 1 == -1 || flagMatrix[x][y - 1]) {
+                            if (x - 1 == -1 || flagMatrix[x - 1][y]) {
+                                break loop;
+                            }
+                            x--;
+                            direction = 3;
+                        } else {
+                            y--;
+                        }
+                        break;
+                    case 3:
+                        if (x - 1 == -1 || flagMatrix[x - 1][y]) {
+                            if (y + 1 == cols || flagMatrix[x][y + 1]) {
+                                break loop;
+                            } else {
+                                y++;
+                            }
+                            direction = 0;
+                        } else {
+                            x--;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return retList;
+        }
+    }
 
-	/**
-	 * 官方题解，避免了switch-case，并且使用for循环直接控制次数，不需要上面的方法中需要两次碰壁之后break
-	 * 
-	 * @param matrix
-	 * @return
-	 */
-	public List<Integer> spiralOrder2(int[][] matrix) {
-		if (matrix.length == 0 || matrix[0].length == 0) {
-			return Collections.emptyList();
-		}
-		List<Integer> ans = new ArrayList<Integer>();
-		int R = matrix.length, C = matrix[0].length;
-		boolean[][] seen = new boolean[R][C];
-		int[] dr = { 0, 1, 0, -1 };
-		int[] dc = { 1, 0, -1, 0 };
-		int r = 0, c = 0, di = 0;
-		for (int i = 0; i < R * C; i++) {
-			ans.add(matrix[r][c]);
-			seen[r][c] = true;
-			int cr = r + dr[di];
-			int cc = c + dc[di];
-			if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]) {
-				r = cr;
-				c = cc;
-			} else {
-				di = (di + 1) % 4;
-				r += dr[di];
-				c += dc[di];
-			}
-		}
-		return ans;
-	}
+    class Solution2 {
+        /**
+         * 官方题解，避免了switch-case，并且使用for循环直接控制次数，不需要上面的方法中需要两次碰壁之后break
+         *
+         * @param matrix
+         * @return
+         */
+        public List<Integer> spiralOrder2(int[][] matrix) {
+            if (matrix.length == 0 || matrix[0].length == 0) {
+                return Collections.emptyList();
+            }
+            List<Integer> ans = new ArrayList<Integer>();
+            int R = matrix.length, C = matrix[0].length;
+            boolean[][] seen = new boolean[R][C];
+            int[] dr = {0, 1, 0, -1};
+            int[] dc = {1, 0, -1, 0};
+            int r = 0, c = 0, di = 0;
+            for (int i = 0; i < R * C; i++) {
+                ans.add(matrix[r][c]);
+                seen[r][c] = true;
+                int cr = r + dr[di];
+                int cc = c + dc[di];
+                if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]) {
+                    r = cr;
+                    c = cc;
+                } else {
+                    di = (di + 1) % 4;
+                    r += dr[di];
+                    c += dc[di];
+                }
+            }
+            return ans;
+        }
+    }
+
+    class Solution3 {
+        public int[] spiralOrder(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return new int[0];
+            }
+            int rows = matrix.length, columns = matrix[0].length;
+            int[] order = new int[rows * columns];
+            int index = 0;
+            int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+            while (left <= right && top <= bottom) {
+                for (int column = left; column <= right; column++) {
+                    order[index++] = matrix[top][column];
+                }
+                for (int row = top + 1; row <= bottom; row++) {
+                    order[index++] = matrix[row][right];
+                }
+                if (left < right && top < bottom) {
+                    for (int column = right - 1; column > left; column--) {
+                        order[index++] = matrix[bottom][column];
+                    }
+                    for (int row = bottom; row > top; row--) {
+                        order[index++] = matrix[row][left];
+                    }
+                }
+                left++;
+                right--;
+                top++;
+                bottom--;
+            }
+            return order;
+        }
+    }
 }
