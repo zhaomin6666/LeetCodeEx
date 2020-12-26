b00a2727bb19e9e278a43120ec0d76b85434c7e6
zm <804122845@qq.com>
2020-12-26 11:17:09 +0800

    增加题目标题

--- algorithms/ex1_100/LEET026.java
+++ algorithms/ex1_100/LEET026.java
@@ -3,11 +3,13 @@ package com.zm.LeetCodeEx.algorithms.ex1_100;
 import com.alibaba.fastjson.JSON;
 
 /**
+ * 26.删除排序数组中的重复项
+ *
  * <b>给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。</b>
- * 
+ *
  * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。</br>
  * </br>
- * 
+ *
  * 示例 1: 给定数组 nums = [1,1,2], 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
  * 你不需要考虑数组中超出新长度后面的元素。 </br>
  * 示例 2: 给定 nums = [0,0,1,1,1,2,2,3,3,4], 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0,
@@ -44,8 +46,9 @@ public class LEET026 {
 		}
 		int l = 1;
 		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] != nums[l - 1])
+			if (nums[i] != nums[l - 1]) {
 				nums[l++] = nums[i];
+			}
 		}
 		return l;
 	}
