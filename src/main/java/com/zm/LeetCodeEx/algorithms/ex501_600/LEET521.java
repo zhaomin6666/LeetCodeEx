44410bbaf9cdc162712cc7af75686861b44f2aa5
zm-worklaptop <804122845@qq.com>
2022-03-05 12:01:25 +0800

    新增521已完成

---
+++ src/main/java/com/zm/LeetCodeEx/algorithms/ex501_600/LEET521.java
@@ -0,0 +1,53 @@
+package com.zm.LeetCodeEx.algorithms.ex501_600;
+
+/**
+ * 521. 最长特殊序列 Ⅰ
+ * 给你两个字符串 a 和 b，请返回 这两个字符串中 最长的特殊序列  。如果不存在，则返回 -1 。
+ * <p>
+ * 「最长特殊序列」 定义如下：该序列为 某字符串独有的最长子序列（即不能是其他字符串的子序列） 。
+ * <p>
+ * 字符串 s 的子序列是在从 s 中删除任意数量的字符后可以获得的字符串。
+ * <p>
+ * 例如，"abc" 是 "aebdc" 的子序列，因为删除 "aebdc" 中斜体加粗的字符可以得到 "abc" 。 "aebdc" 的子序列还包括 "aebdc" 、 "aeb" 和 "" (空字符串)。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: a = "aba", b = "cdc"
+ * 输出: 3
+ * 解释: 最长特殊序列可为 "aba" (或 "cdc")，两者均为自身的子序列且不是对方的子序列。
+ * 示例 2：
+ * <p>
+ * 输入：a = "aaa", b = "bbb"
+ * 输出：3
+ * 解释: 最长特殊序列是 "aaa" 和 "bbb" 。
+ * 示例 3：
+ * <p>
+ * 输入：a = "aaa", b = "aaa"
+ * 输出：-1
+ * 解释: 字符串 a 的每个子序列也是字符串 b 的每个子序列。同样，字符串 b 的每个子序列也是字符串 a 的子序列。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= a.length, b.length <= 100
+ * a 和 b 由小写英文字母组成
+ *
+ * @author zm
+ */
+public class LEET521 {
+	public static void main(String[] args) {
+		System.out.println(new Solution().findLUSlength("aba", "cdc"));
+		System.out.println(new Solution().findLUSlength("aaa", "bbb"));
+		System.out.println(new Solution().findLUSlength("aaa", "aaa"));
+	}
+
+	/**
+	 * 如果两个字符串不一样，那么直接去较长的那个，如果一样则说明没有特殊序列
+	 */
+	static class Solution {
+		public int findLUSlength(String a, String b) {
+			return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+		}
+	}
+}
